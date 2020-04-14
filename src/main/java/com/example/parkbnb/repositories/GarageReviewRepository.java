0dc811ca1c5e39eee18e0e8113b4271f6938e4e7
Michael <56820868+y458@users.noreply.github.com>
2020-04-14 15:50:56 +0300

    Add files via upload

---
+++ src/main/java/com/example/parkbnb/repositories/GarageReviewRepository.java
@@ -0,0 +1,10 @@
+
+package com.example.parkbnb.repositories;
+
+import com.example.parkbnb.models.GarageReview;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface GarageReviewRepository extends CrudRepository<GarageReview, Integer>{
+    
+}
